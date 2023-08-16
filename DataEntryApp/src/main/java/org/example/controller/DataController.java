465b9d82095c28724eeb4fe3cd8e019c8fbc0d02
Jennifer Trevis <jennifertrevis@Jennifers-MacBook-Pro.local>
2023-08-16 16:35:14 -0400

    Starting on Frontend Setup

--- DataEntryApp/src/main/java/org/example/controller/DataController.java
+++ DataEntryApp/src/main/java/org/example/controller/DataController.java
@@ -1,9 +1,24 @@
 package org.example.controller;
 
+import org.example.dao.DataDao;
+import org.example.model.Data;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 public class DataController {
+
+    private final DataDao dataDao;
+
+    public DataController(DataDao dataDao) {
+        this.dataDao = dataDao;
+    }
+
+    @RequestMapping(path = "/users", method = RequestMethod.GET)
+    public List<Data> listAllData () { return dataDao.findall();}
 }
--- DataEntryApp/src/main/java/org/example/dao/JdbcDataDao.java
+++ DataEntryApp/src/main/java/org/example/dao/JdbcDataDao.java
@@ -2,15 +2,41 @@ package org.example.dao;
 
 
 import org.example.model.Data;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
 @Component
 public class JdbcDataDao implements DataDao{
 
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcDataDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public List<Data> findall() {
-        return null;
+        List<Data> users = new ArrayList<>();
+        String sql = "SELECT * from user_data";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+        while (results.next()){
+            Data user = mapRowToUser(results);
+            users.add(user);
+        }
+        return users;
+    }
+
+    private Data mapRowToUser(SqlRowSet rs) {
+        Data user = new Data();
+        user.setName(rs.getString("user_name"));
+        user.setAge(rs.getInt("user_age"));
+        user.setTitle(rs.getString("user_title"));
+        user.setHometown(rs.getString("user_hometown"));
+        return user;
     }
 }
